0a6ff140540b979d26f673e09c08ba153b2e02f9
Brian-bccm <167698555+Brian-bccm@users.noreply.github.com>
2025-05-07 22:35:06 +0800

    Update constants.php

--- config/constants.php
+++ config/constants.php
@@ -5,9 +5,9 @@
 
     //Create Constants to Store Non Repeating Values
     define('SITEURL','http://localhost/wushu-society/');
-    define('LOCALHOST','localhost');
-    define('DB_USERNAME','root');
-    define('DB_PASSWORD','');
+    define('LOCALHOST','lab-db.cqrei3axetnf.us-east-1.rds.amazonaws.com');
+    define('DB_USERNAME','lab-db');
+    define('DB_PASSWORD','admin1234');
     define('DB_NAME','wushu-society');
 
     $conn = new mysqli(LOCALHOST, DB_USERNAME, DB_PASSWORD, DB_NAME); //Database Connection
